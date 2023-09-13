c3892b22f79ffc41cc65e8d926c89e183965051c
Yin-FR <tanyin625@gmail.com>
2023-09-13 23:07:28 +0200

    delete api

--- api/yin_resume_api/bussiness_logical/bl_project.py
+++
@@ -1,7 +0,0 @@
-from yin_resume_api.orm_mongodb.project import Project
-from flask import jsonify
-
-
-def bl_get_all_projects(**kwargs):
-    projects = Project.objects(**kwargs)
-    return jsonify(projects), 200
--- api/yin_resume_api/orm_mongodb/social_network.py
+++
@@ -1,9 +0,0 @@
-from yin_resume_api.orm_mongodb import db
-
-
-class Social_Network(db.Document):
-    name_id = db.StringField()
-    name = db.StringField()
-    className = db.StringField()
-    url = db.StringField()
-    meta = {'collection': 'social_network'}
